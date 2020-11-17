3291fc1a90256976204e7d8fbcc5f384ddf98bd9
blankqwq <1136589038@qq.com>
2020-11-17 11:47:33 +0800

    add user namespace

--- day-1-LinuxNamespace/main/main.go
+++ day-1-LinuxNamespace/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -10,9 +11,25 @@ import (
 func main() {
 	// fork a thread to run sh
 	cmd := exec.Command("sh")
-	// UTS NAMESPACE and IPC NAMESPACE and PIC NAMESPACE
+	// UTS NAMESPACE and IPC NAMESPACE and PIC NAMESPACE and USER NAMESPACE and NETWORK
+	//centos 7 use USER NAMESPACE must run grubby --args="user_namespace.enable=1" --update-kernel="$(grubby --default-kernel)"
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags :syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC |syscall.CLONE_NEWPID |syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET| syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      syscall.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      syscall.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -21,4 +38,5 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
+	os.Exit(-1)
 }
