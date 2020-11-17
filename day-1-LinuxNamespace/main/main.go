5285acff5a87ebc024db370d0b4ed84a26276227
blankqwq <1136589038@qq.com>
2020-11-17 11:22:47 +0800

    add new namespace

--- day-1-LinuxNamespace/main/main.go
+++ day-1-LinuxNamespace/main/main.go
@@ -8,9 +8,11 @@ import (
 )
 
 func main() {
+	// fork a thread to run sh
 	cmd := exec.Command("sh")
+	// UTS NAMESPACE and IPC NAMESPACE and PIC NAMESPACE
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags :syscall.CLONE_NEWUTS,
+		Cloneflags :syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC |syscall.CLONE_NEWPID |syscall.CLONE_NEWNS,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
